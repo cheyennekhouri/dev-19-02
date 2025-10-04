b27a267e912c1ed47068c905d24ac3eee193ce72
Cheyenne <cheyenne.r.khouri@gmail.com>
2025-10-04 10:07:05 -0700

    Changed ProgrammingLanguage to Main

--- src/main/java/com/example/dev1902/Main.java
+++ src/main/java/com/example/dev1902/Main.java
@@ -8,18 +8,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 //programming language page
-public class ProgrammingLanguage extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
         //displays popup with button to launch app
-        FXMLLoader fxmlLoader = new FXMLLoader(ProgrammingLanguage.class.getResource("program-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Launch KnowledgeTrack App");
         stage.setScene(scene);
         stage.show();
 
-        FXMLLoader fxmlLoader1 = new FXMLLoader(ProgrammingLanguage.class.getResource("program-view.fxml"));
+        FXMLLoader fxmlLoader1 = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene1 = new Scene(fxmlLoader1.load(), 320, 240);
         stage.setTitle("Navigate to Programming Languages Tab");
         stage.setScene(scene1);
--- src/main/java/com/example/dev1902/MainController.java
+++ src/main/java/com/example/dev1902/MainController.java
@@ -3,7 +3,7 @@ package com.example.dev1902;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class ProgrammingLanguageControl {
+public class MainController {
     @FXML
     private Label popUp;
     public Label popUp2;
